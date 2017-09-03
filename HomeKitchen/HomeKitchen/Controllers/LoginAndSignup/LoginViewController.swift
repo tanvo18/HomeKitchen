44d7a03832ac8728b309401ae5ab0ca7bc1c8fcf
Tan Vo <tanvo.bks2014@gmail.com>
2017-09-03 17:18:53 +0700

    login-and-signup

---
+++ HomeKitchen/HomeKitchen/Controllers/LoginAndSignup/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  HomeKitchen
+//
+//  Created by Tan Vo on 9/3/17.
+//  Copyright © 2017 Tan Vo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  @IBOutlet weak var usernameTextfield: UITextField!
+  
+  @IBOutlet weak var passwordTextfield: UITextField!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupTextfield()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+}
+
+extension LoginViewController {
+  func setupTextfield() {
+    let paddingViewUser = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.usernameTextfield.frame.height))
+    usernameTextfield.leftView = paddingViewUser
+    usernameTextfield.leftViewMode = UITextFieldViewMode.always
+    let paddingViewPassword = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.passwordTextfield.frame.height))
+    passwordTextfield.leftView = paddingViewPassword
+    passwordTextfield.leftViewMode = UITextFieldViewMode.always
+  }
+}
---
+++ HomeKitchen/HomeKitchen/Controllers/LoginAndSignup/SignupViewController.swift
@@ -0,0 +1,48 @@
+//
+//  SignupViewController.swift
+//  HomeKitchen
+//
+//  Created by Tan Vo on 9/3/17.
+//  Copyright © 2017 Tan Vo. All rights reserved.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+
+  @IBOutlet weak var usernameTextField: UITextField!
+  
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  @IBOutlet weak var confirmPassTextField: UITextField!
+  
+  @IBOutlet weak var phoneTextField: UITextField!
+  
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTextfield()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+  
+  func setupTextfield() {
+    let paddingViewUser = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.usernameTextField.frame.height))
+    usernameTextField.leftView = paddingViewUser
+    usernameTextField.leftViewMode = UITextFieldViewMode.always
+    
+    let paddingViewPass = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.passwordTextField.frame.height))
+    passwordTextField.leftView = paddingViewPass
+    passwordTextField.leftViewMode = UITextFieldViewMode.always
+    
+    let paddingViewConfirmPass = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.confirmPassTextField.frame.height))
+    confirmPassTextField.leftView = paddingViewConfirmPass
+    confirmPassTextField.leftViewMode = UITextFieldViewMode.always
+    
+    let paddingViewUserPhone = UIView(frame: CGRect(x: 0, y: 0, width: 30, height: self.phoneTextField.frame.height))
+    phoneTextField.leftView = paddingViewUserPhone
+    phoneTextField.leftViewMode = UITextFieldViewMode.always
+  }
+}
